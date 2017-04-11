b102be1d97d7dab3d83949077d9cef8725cc6c95
· Aquila · <apocalaeon@hotmail.com>
2017-04-11 18:34:29 +1200

    general layout and UI elements added, webview activity created but not implemented

--- app/src/main/java/com/example/aquila/mobileassignment1/WebViewActivity.java
+++ app/src/main/java/com/example/aquila/mobileassignment1/WebViewActivity.java
@@ -3,15 +3,11 @@ package com.example.aquila.mobileassignment1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class WebViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-/*
-        getActionBar().setIcon(R.drawable.logo);
-*/
-
+        setContentView(R.layout.activity_web_view);
     }
 }
